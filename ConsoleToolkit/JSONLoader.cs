ec9dbdaeb66e5db7c95c5d6dc9981188757b2f45
safranek2 <safranek2@spsejecna.cz>
2024-04-15 21:50:43 +0200

    Folder Rename

---
+++ ConsoleToolkit/JSONLoader.cs
@@ -0,0 +1,189 @@
+﻿using ConsoleToolkit.Randomizer.JSON_Classes;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleToolkit
+{
+    /// <summary>
+    /// Třída pro načítání dat ze souborů ve formátu JSON.
+    /// </summary>
+    internal class JSONLoader
+    {
+        private readonly ErrorController errorController = new ErrorController(); // Instance třídy pro správu chyb
+        private readonly string countriesPath = Path.Combine("Resources", "Countries.json"); // Cesta k JSON souboru se zeměmi
+        private readonly string dicesSettingsPath = Path.Combine("Resources", "DicesSettings.json"); // Cesta k JSON souboru s nastavením kostek
+        private readonly string alphabetsPath = Path.Combine("Resources", "Alphabets.json"); // Cesta k JSON souboru s abecedami
+        private readonly string magic8BallAnswersPath = Path.Combine("Resources", "Magic8BallAnswers.json"); // Cesta k JSON souboru s odpověďmi Magic 8 Ball
+        private readonly string numbersSettingsPath = Path.Combine("Resources", "NumbersSettings.json"); // Cesta k JSON souboru s nastavením čísel
+        private readonly string passwordSettingsPath = Path.Combine("Resources", "PasswordSettings.json"); // Cesta k JSON souboru s nastavením hesla
+        private readonly string teamsSettingsPath = Path.Combine("Resources", "TeamsSettings.json"); // Cesta k JSON souboru s nastavením teamů
+
+        /// <summary>
+        /// Metoda pro načítání seznamu zemí ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Seznam zemí nebo null v případě chyby.</returns>
+        public List<string> LoadCountriesFromJSON()
+        {
+            try
+            {
+                // Načtení obsahu souboru do textového řetězce
+                string jsonContent = File.ReadAllText(countriesPath);
+
+                // Deserializace JSON obsahu do objektu třídy Countries
+                Countries countries = JsonConvert.DeserializeObject<Countries>(jsonContent);
+
+                // Vrácení seznamu zemí z objektu Countries
+                return countries.CountryList;
+            }
+            catch (Exception ex)
+            {
+                // Pokud došlo k chybě při načítání souboru, vypiš chybovou zprávu a vrať null
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání nastavení pro házení kostek ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Objekt DiceSettings obsahující nastavení kostek nebo null v případě chyby.</returns>
+        public DiceSettings LoadDicesSettingsFromJSON()
+        {
+            try
+            {
+                // Načtení obsahu souboru do textového řetězce
+                string jsonContent = File.ReadAllText(dicesSettingsPath);
+
+                // Deserializace JSON obsahu do objektu třídy DiceSettings
+                DiceSettings dicesSettings = JsonConvert.DeserializeObject<DiceSettings>(jsonContent);
+
+                return dicesSettings;
+            }
+            catch (Exception ex)
+            {
+                // Pokud došlo k chybě při načítání souboru, vypiš chybovou zprávu a vrať null
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání abeced ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Slovník obsahující abecedy nebo null v případě chyby.</returns>
+        public Dictionary<string, string> LoadAlphabetsFromJSON()
+        {
+            try
+            {
+                // Načtení obsahu souboru do textového řetězce
+                string jsonContent = File.ReadAllText(alphabetsPath);
+
+                // Deserializace JSON obsahu do slovníku abeced
+                Alphabets alphabets = JsonConvert.DeserializeObject<Alphabets>(jsonContent);
+
+                return alphabets.AlphabetList;
+            }
+            catch (Exception ex)
+            {
+                // Pokud došlo k chybě při načítání souboru, vypiš chybovou zprávu a vrať null
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání odpovědí Magic 8 Ball ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Seznam možných odpovědí Magic 8 Ball nebo null v případě chyby.</returns>
+        public List<string> LoadMagic8BallAnswersFromJSON()
+        {
+            try
+            {
+                // Načtení obsahu souboru do textového řetězce
+                string jsonContent = File.ReadAllText(magic8BallAnswersPath);
+
+                // Deserializace JSON obsahu do objektu třídy Magic8BallAnswers
+                Magic8BallAnswers magic8BallAnswers = JsonConvert.DeserializeObject<Magic8BallAnswers>(jsonContent);
+
+                return magic8BallAnswers.Answers;
+            }
+            catch (Exception ex)
+            {
+                // Pokud došlo k chybě při načítání souboru, vypiš chybovou zprávu a vrať null
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání nastavení čísel ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Maximální počet čísel nebo -1 v případě chyby.</returns>
+        public int LoadNumbersSettingsFromJSON()
+        {
+            try
+            {
+                // Načtení obsahu souboru do textového řetězce
+                string jsonContent = File.ReadAllText(numbersSettingsPath);
+
+                // Deserializace JSON obsahu do objektu třídy Numbers
+                NumbersSettings numbersSettings = JsonConvert.DeserializeObject<NumbersSettings>(jsonContent);
+
+                return numbersSettings.MaxNumbers;
+            }
+            catch (Exception ex)
+            {
+                // Pokud došlo k chybě při načítání souboru, vypiš chybovou zprávu a vrať -1
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání nastavení hesla ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Objekt PasswordSettings obsahující nastavení hesla nebo null v případě chyby.</returns>
+        public PasswordSettings LoadPasswordSettingsFromJSON()
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(passwordSettingsPath);
+                PasswordSettings passwordSettings = JsonConvert.DeserializeObject<PasswordSettings>(jsonContent);
+                return passwordSettings;
+            }
+            catch (Exception ex)
+            {
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Metoda pro načítání nastavení týmů ze souboru ve formátu JSON.
+        /// </summary>
+        /// <returns>Objekt TeamsSettings obsahující nastavení hesla nebo null v případě chyby.</returns>
+        public TeamsSettings LoadTeamsSettingsFromJSON()
+        {
+            try
+            {
+                string jsonContent = File.ReadAllText(teamsSettingsPath);
+                TeamsSettings teamsSettings = JsonConvert.DeserializeObject<TeamsSettings>(jsonContent);
+                return teamsSettings;
+            }
+            catch (Exception ex)
+            {
+                errorController.PrintError($"Error when reading a file: {ex.Message}");
+                Environment.Exit(0);
+                return null;
+            }
+        }
+    }
+}
---
+++ ConsoleToolkit/Randomizer/ColorGenerator.cs
@@ -0,0 +1,85 @@
+﻿using ConsoleToolkit.vendor; // Importuje vendor pro ClosestConsoleColor
+using System;
+
+namespace ConsoleToolkit
+{
+    /// <summary>
+    /// Třída pro generování náhodných barev a hledání nejbližší konzolové barvy.
+    /// </summary>
+    internal class ColorGenerator
+    {
+        /// <summary>
+        /// Konstruktor třídy ColorGenerator.
+        /// Inicializuje instanci třídy ClosestConsoleColor.
+        /// </summary>
+        public ColorGenerator()
+        {
+            closestConsoleColor = new ClosestConsoleColor();
+        }
+
+        private readonly Random random = new Random(); // Instance třídy pro generování náhodných čísel
+
+        private readonly ClosestConsoleColor closestConsoleColor; // Instance třídy pro nalezení nejbližší barvy v konzoli
+
+
+        /// <summary>
+        /// Metoda pro zobrazení náhodné barvy a nejbližší konzolové barvy.
+        /// </summary>
+        public void Display()
+        {
+            // Generuje náhodnou barvu
+            string color = GenerateRandomColor();
+
+            // Vypočítá nejbližší barvu v konzoli
+            ConsoleColor closestColor = closestConsoleColor.Calculate(color);
+
+            // Vypíše náhodnou barvu
+            Console.WriteLine("Random color: " + color);
+
+            // Nastaví barvu pozadí konzole na nejbližší barvu
+            SetConsoleBackgroundColor(closestColor);
+
+            // Vypíše informaci o nejbližší konzolové barvě
+            Console.WriteLine($"The closest console color to {color} is {closestColor}");
+
+            // Resetuje barvu pozadí konzole zpět na černou
+            ResetConsoleBackgroundColor();
+        }
+
+        /// <summary>
+        /// Metoda pro generování náhodné barvy v hexadecimálním formátu.
+        /// </summary>
+        /// <returns>Řetězec představující náhodnou barvu v hexadecimálním formátu.</returns>
+        private string GenerateRandomColor()
+        {
+            // Generuje náhodné hodnoty pro složky RGB
+            int red = random.Next(256);
+            int green = random.Next(256);
+            int blue = random.Next(256);
+
+            // Sestaví hexadecimální reprezentaci barvy
+            string color = $"#" + red.ToString("X2") + green.ToString("X2") + blue.ToString("X2");
+
+            return color;
+        }
+
+        /// <summary>
+        /// Metoda pro nastavení barvy pozadí konzole.
+        /// </summary>
+        /// <param name="color">Barva, která se má nastavit jako pozadí konzole.</param>
+        private void SetConsoleBackgroundColor(ConsoleColor color)
+        {
+            // Nastaví barvu pozadí konzole podle parametru color
+            Console.BackgroundColor = color;
+        }
+
+        /// <summary>
+        /// Metoda pro resetování barvy pozadí konzole na černou.
+        /// </summary>
+        private void ResetConsoleBackgroundColor()
+        {
+            // Nastaví barvu pozadí konzole na černou
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
